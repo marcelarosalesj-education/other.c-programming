d986c80e043156708e3546f03015e2a1eb2212e5
Marcela Rosales <marcelarosalesj@gmail.com>
2017-05-25 00:56:57 -0500

    Fixed some errors. Now matrix creation seems fine. Printing sometimes works, I'm still debugging it.
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- Matrix/matrix.c
+++ Matrix/matrix.c
@@ -12,7 +12,7 @@ struct node{
   struct node *col;
 };
 
-struct node* CreateRandomMatrix(int rows, int cols){
+struct node* CreateRandomMatrix(int rows, int cols) {
   int i,j, control=0;
   double max = 100.0, min=1.0;
   double factor = (max - min)/RAND_MAX;
@@ -20,24 +20,29 @@ struct node* CreateRandomMatrix(int rows, int cols){
 
   struct node* this = origin;
   (*this).val=control;//factor*rand();
+  control+=10;
   (*this).row = NULL;
   (*this).col = NULL;
 
   struct node* cpythis = this;
 
   for(i=0; i<rows; i++){
-    for(j=1; j<=cols; j++){
+    for(j=0; j<cols-1; j++){
 
       struct node* newnode = (struct node*)malloc(sizeof(struct node));
       (*newnode).val=control;//factor*rand();
-
       control+=10;
       (*newnode).row = NULL;
       (*newnode).col = NULL;
       (*this).col = newnode;
-      printf("this(%p):%d->%p , new(%p):%d->%p \n", (&this) , (*this).val , &(*this).col,
-                                &(*this).col, (*(*this).col).val , &(*(*this).col).col );
-      this=newnode;
+      printf("this(%p):%d->%p , new(%p):%d->%p \n", this , this->val , this->col,
+                                this->col, this->col->val , this->col->col);
+      
+      if(j < cols ){
+        this=newnode;
+      }
+      
+
       /*
       (*this+i*cols+j).val=factor*rand();
       (*this+i*cols+j).row = nextrow;
@@ -47,18 +52,25 @@ struct node* CreateRandomMatrix(int rows, int cols){
       thiscol = (*thisrow+i*cols+j+1);
       nextcol = thiscol+1;*/
     }
-
-    if(i<rows){
-      this = cpythis;
+    printf("i is = %d\n",i );
+    if(i<rows-1){
+      this = cpythis; // go back to the begining
       printf("\n");
-      struct node* newnode = (struct node*)malloc(sizeof(struct node));
-      (*newnode).val=factor*rand();
-      (*newnode).row = NULL;
-      (*newnode).col = NULL;
-      (*this).row = newnode;
-      this = (*this).row;
+      struct node* newnode2 = (struct node*)malloc(sizeof(struct node));
+      (*newnode2).val=control;//factor*rand();
+      control+=10;
+      (*newnode2).row = NULL;
+      (*newnode2).col = NULL;
+      (*this).row = newnode2;
+      this = this->row;
+      cpythis = this;
     }
   }
+
+
+
+printf("\nORIGIN(%p):%d->%p , next(%p):%d->%p \n", origin , origin->val , origin->col,
+                                origin->col, origin->col->val , origin->col->col);
 
   return origin;
 
@@ -68,21 +80,30 @@ void PrintMatrix(struct node* ptr){
   bool finish = false;
 
   struct node* cpy = ptr;
-  while(!finish) {
+  printf("addr inicio : %p (%d) \n", ptr, ptr->val);
+  printf("second row : %p (%d) \n", cpy->row, cpy->row->val );
 
-    if((*ptr).val != NULL){
-      printf("nn %d ", (*ptr).val);
-      ptr=(*ptr).col;
-    } else {
-      printf("enter \n");
-      ptr = (*cpy).row;
-      cpy=ptr;
-      if((*cpy).val == NULL){
-        finish=true;
-      }
+  while( !finish ) {
+
+    // printing first row, every column
+    while( ptr != NULL ) {
+      printf(" ( %d ) ->", ptr->val);
+      ptr = ptr->col;
     }
 
-
+    printf(" NULL \n");
+    printf("   |        |        | \n");
+    
+    // next row
+    ptr = cpy->row;
+    cpy = ptr;
+    
+    if( ptr != NULL) {
+      // repete printing row
+    } else {
+      finish = true;
+      printf("  NULL     NULL     NUL\n");  
+    }
 
   }
 
@@ -93,13 +114,20 @@ bool DeleteMatrix(struct node* origin){
 
 }
 
-int main(){
+int main(int argc, char *argv[]){
+  
+  if(argc < 3){
+    printf("Call: <file> <num of rows> <num of columns>\n");
+    return 1;
+  }
+
   struct node *n1;
   n1 = (struct node*)malloc(sizeof(struct node));
-  n1 = CreateRandomMatrix(2,3); // 2 rows, 3 cols
+  n1 = CreateRandomMatrix(atoi(argv[1]), atoi(argv[2])); // 2 rows, 3 cols
+  
 
-  printf("created matrix %d , %d , %d\n", (*n1).val, *(*n1).col, *(*n1).row);
-  //PrintMatrix(n1);
+  printf("Create Matrix of n1( %p )\n", n1);
+  PrintMatrix(n1);
 
   return 0;
 }
